ef8e70073ce513af045eb345527d2ce2042b1c65
abha566 <showabha8@gmail.com>
2026-01-27 13:22:21 +0530

    Add files via upload

---
+++ aha.py
@@ -0,0 +1,27 @@
+tuple1=(12,3,5,8)
+tuple2=(6,7,9,10)
+c=list(tuple1)
+c[2]=6
+a=tuple(c)
+print(a)
+
+
+a=(2,3,8,4,5)
+print(a[1:])
+print(24,)
+print(type(24,))
+
+
+
+a=[2,6,7,12,19,3,4,34,9,8]
+print(a[2:])
+print(a[1:6:1])
+print(a[-1:0:-1])
+
+name=input("Enter your name-")
+print(name)
+name[0]='tina'
+d=str(name)
+
+
+  
